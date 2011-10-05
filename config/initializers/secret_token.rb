20a31c6a18beceb922b8410b5c6dff3550315358
Durga Prasad <durgaprasad.b@nyros.in>
2011-10-05 16:39:57 +0530

    intilize application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callbacks::Application.config.secret_token = '31a52cc5f71586265fd2daf6d8342c907a0ca047e6ec2a2560df9159a330303deab21b48d0d7fc2813c3f723ead96c9c3be610490e18e939110470dac0a0965b'
